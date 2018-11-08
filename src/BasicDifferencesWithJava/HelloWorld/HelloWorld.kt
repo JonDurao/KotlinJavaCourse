c12d49f9c292b97f034a16dbf79fc8a1494f2d8f
Jon Durao <j.durao@spexx.com>
2018-11-08 17:32:38 +0100

    Hello World

--- src/BasicDifferencesWithJava/HelloWorld/HelloWorld.kt
+++ src/BasicDifferencesWithJava/HelloWorld/HelloWorld.kt
@@ -1,8 +1,21 @@
 package BasicDifferencesWithJava.HelloWorld
 
-// COMPILE KOTLIN
-// Compiler takes files of type .kt and generates bytecode as .class, java files, that'll be runned by the JVM
+/**
+ *  COMPILE KOTLIN
+ *  Compiler takes files of type .kt and generates bytecode as .class, java files, that'll be runned by the JVM
+ **/
+
+/**
+ * IMPORTS
+ * A lot of classes are imported by default, called KOTLIN STANDARD LIBRARY
+ *
+ */
+
+/**
+ *  FUNCTIONS OUTSIDE CLASSES -> TOP-LEVEL FUNCTION
+ *  Functions can be outside classes, the compiler takes care of it
+ */
 
 fun main(args: Array<String>) {
-
+    println("Hello World!")
 }
