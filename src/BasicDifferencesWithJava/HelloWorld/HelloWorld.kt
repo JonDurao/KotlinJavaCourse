bdd234af93612dc830f97497bd8d5110c43c7adb
Jon Durao <j.durao@spexx.com>
2018-11-08 17:22:31 +0100

    Hello World

---
+++ src/BasicDifferencesWithJava/HelloWorld/HelloWorld.kt
@@ -0,0 +1,8 @@
+package BasicDifferencesWithJava.HelloWorld
+
+// COMPILE KOTLIN
+// Compiler takes files of type .kt and generates bytecode as .class, java files, that'll be runned by the JVM
+
+fun main(args: Array<String>) {
+
+}
